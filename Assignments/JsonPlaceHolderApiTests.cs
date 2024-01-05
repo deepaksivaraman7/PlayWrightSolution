25e1a982e897d374ba2c45c258dc205b6aef6007
deepaksivaraman7 <deepak.sivaraman@ust.com>
2024-01-05 15:29:30 +0530

    05-01-2024

--- Assignments/JsonPlaceHolderApiTests.cs
+++ Assignments/JsonPlaceHolderApiTests.cs
@@ -1,9 +1,14 @@
-using Microsoft.Playwright;
+﻿using Microsoft.Playwright;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Text.Json;
+using System.Threading.Tasks;
 
-namespace PlayWrightAPI
+namespace Assignments
 {
-    public class ReqResApiTests
+    internal class JsonPlaceHolderApiTests
     {
         IAPIRequestContext requestContext;
         [SetUp]
@@ -13,15 +18,15 @@ namespace PlayWrightAPI
             requestContext = await playWright.APIRequest.NewContextAsync(
                 new APIRequestNewContextOptions
                 {
-                    BaseURL = "https://reqres.in/api/",
+                    BaseURL = "https://jsonplaceholder.typicode.com/",
                     IgnoreHTTPSErrors = true
                 });
         }
 
-        [Test, TestCase(2)]
-        public async Task GetAllUsers(int pageNo)
+        [Test]
+        public async Task GetAllPosts()
         {
-            var getResponse = await requestContext.GetAsync(url: "users?page=" + pageNo);
+            var getResponse = await requestContext.GetAsync(url: "posts");
             await Console.Out.WriteLineAsync("Response: " + getResponse.ToString());
             await Console.Out.WriteLineAsync("Code: " + getResponse.Status);
             await Console.Out.WriteLineAsync("Text: " + getResponse.StatusText);
@@ -35,9 +40,9 @@ namespace PlayWrightAPI
         }
 
         [Test, TestCase(2)]
-        public async Task GetSingleUser(int id)
+        public async Task GetSinglePost(int id)
         {
-            var getResponse = await requestContext.GetAsync(url: "users/" + id);
+            var getResponse = await requestContext.GetAsync(url: "posts/" + id);
             await Console.Out.WriteLineAsync("Response: " + getResponse.ToString());
             await Console.Out.WriteLineAsync("Code: " + getResponse.Status);
             await Console.Out.WriteLineAsync("Text: " + getResponse.StatusText);
@@ -50,34 +55,19 @@ namespace PlayWrightAPI
 
         }
 
-        [Test, TestCase(23)]
-        public async Task GetSingleUserNotFound(int id)
-        {
-            var getResponse = await requestContext.GetAsync(url: "users/" + id);
-            await Console.Out.WriteLineAsync("Response: " + getResponse.ToString());
-            await Console.Out.WriteLineAsync("Code: " + getResponse.Status);
-            await Console.Out.WriteLineAsync("Text: " + getResponse.StatusText);
-
-            Assert.That(getResponse.Status, Is.EqualTo(404));
-            Assert.That(getResponse, Is.Not.Null);
-
-            JsonElement responseBody = (JsonElement)await getResponse.JsonAsync();
-            await Console.Out.WriteLineAsync("Response body: " + responseBody.ToString());
-            Assert.That(responseBody.ToString(), Is.EqualTo("{}"));
-        }
-
-        [Test, TestCase("John", "Engineer")]
-        public async Task PostUser(string newName, string newJob)
+        [Test, TestCase("New title", "New body")]
+        public async Task CreatePost(string newTitle, string newBody)
         {
             var postData = new
             {
-                name = newName,
-                job = newJob
+                userId = 3,
+                title = newTitle,
+                body = newBody
             };
             var jsonPostData = JsonSerializer.Serialize(postData);
-            var postResponse = await requestContext.PostAsync(url: "users", new APIRequestContextOptions
+            var postResponse = await requestContext.PostAsync(url: "posts", new APIRequestContextOptions
             {
-                Data = jsonPostData
+                //Data = jsonPostData
             });
             await Console.Out.WriteLineAsync("Response: " + postResponse.ToString());
             await Console.Out.WriteLineAsync("Code: " + postResponse.Status);
@@ -91,16 +81,17 @@ namespace PlayWrightAPI
             Assert.That(responseBody.ToString(), Is.Not.Empty);
         }
 
-        [Test, TestCase(2, "John", "Engineer")]
-        public async Task PutUser(int id, string updatedName, string updatedJob)
+        [Test, TestCase(2,3, "Updated title", "Updated body")]
+        public async Task PutPost(int id,int userID, string updatedTitle, string updatedBody)
         {
             var putData = new
             {
-                name = updatedName,
-                job = updatedJob
+                userId = userID,
+                title = updatedTitle,
+                body = updatedBody
             };
             var jsonPutData = JsonSerializer.Serialize(putData);
-            var putResponse = await requestContext.PutAsync(url: "users/" + id, new APIRequestContextOptions
+            var putResponse = await requestContext.PutAsync(url: "posts/" + id, new APIRequestContextOptions
             {
                 //Data = jsonPutData
             });
@@ -117,14 +108,14 @@ namespace PlayWrightAPI
         }
 
         [Test, TestCase(2)]
-        public async Task DeleteUser(int id)
+        public async Task DeletePost(int id)
         {
-            var deleteResponse = await requestContext.DeleteAsync(url: "users/" + id);
+            var deleteResponse = await requestContext.DeleteAsync(url: "posts/" + id);
             await Console.Out.WriteLineAsync("Response: " + deleteResponse.ToString());
             await Console.Out.WriteLineAsync("Code: " + deleteResponse.Status);
             await Console.Out.WriteLineAsync("Text: " + deleteResponse.StatusText);
 
-            Assert.That(deleteResponse.Status, Is.EqualTo(204));
+            Assert.That(deleteResponse.Status, Is.EqualTo(200));
         }
     }
 }
